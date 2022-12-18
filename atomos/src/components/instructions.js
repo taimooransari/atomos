173f4d5f6c1b2a7f4c7a071896caba088d5e2ace
Taimoor Ansari <ma07595@st.habib.edu.pk>
2022-12-19 00:46:37 +0500

    instructions added

--- atomos/src/components/instructions.js
+++ atomos/src/components/instructions.js
@@ -3,7 +3,7 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
 function Instructions() {
-    const values = [true, 'sm-down', 'md-down', 'lg-down', 'xl-down', 'xxl-down'];
+    // const values = [true, 'sm-down', 'md-down', 'lg-down', 'xl-down', 'xxl-down'];
     const [fullscreen, setFullscreen] = useState(true);
     const [show, setShow] = useState(false);
 
@@ -15,11 +15,11 @@ function Instructions() {
     return (
         <>
             {/* {values.map((v, idx) => ( */}
-                <Button className="me-2 mb-2" onClick={() => handleShow('sm-down')}>
-                    SHOW INSTRUCTIONS
-                    {/* Full screen
+            <Button className="me-2 mb-2" onClick={() => handleShow('sm-down')}>
+                SHOW INSTRUCTIONS
+                {/* Full screen
                     {typeof v === 'string' && `below ${v.split('-')[0]}`} */}
-                </Button>
+            </Button>
             {/* ))} */}
             <Modal show={show} fullscreen={fullscreen} onHide={() => setShow(false)}>
                 <Modal.Header closeButton>
@@ -27,17 +27,49 @@ function Instructions() {
                 </Modal.Header>
                 <Modal.Body>Instructions body content
 
-                    <ul>
-                        <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae, nihil! Non, iure id autem incidunt voluptatibus eius! Ipsum sunt, odit veritatis veniam quis omnis cumque inventore quaerat quas mollitia debitis.</li>
-                        <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae, nihil! Non, iure id autem incidunt voluptatibus eius! Ipsum sunt, odit veritatis veniam quis omnis cumque inventore quaerat quas mollitia debitis.</li>
-                        <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae, nihil! Non, iure id autem incidunt voluptatibus eius! Ipsum sunt, odit veritatis veniam quis omnis cumque inventore quaerat quas mollitia debitis.</li>
-                        <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae, nihil! Non, iure id autem incidunt voluptatibus eius! Ipsum sunt, odit veritatis veniam quis omnis cumque inventore quaerat quas mollitia debitis.</li>
-                        <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae, nihil! Non, iure id autem incidunt voluptatibus eius! Ipsum sunt, odit veritatis veniam quis omnis cumque inventore quaerat quas mollitia debitis.</li>
-                        <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae, nihil! Non, iure id autem incidunt voluptatibus eius! Ipsum sunt, odit veritatis veniam quis omnis cumque inventore quaerat quas mollitia debitis.</li>
-                        <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae, nihil! Non, iure id autem incidunt voluptatibus eius! Ipsum sunt, odit veritatis veniam quis omnis cumque inventore quaerat quas mollitia debitis.</li>
-                        <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae, nihil! Non, iure id autem incidunt voluptatibus eius! Ipsum sunt, odit veritatis veniam quis omnis cumque inventore quaerat quas mollitia debitis.</li>
-                        <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae, nihil! Non, iure id autem incidunt voluptatibus eius! Ipsum sunt, odit veritatis veniam quis omnis cumque inventore quaerat quas mollitia debitis.</li>
-                    </ul>
+                    <ol>
+
+                        <li>A team can be of 5 to 7 members including the team lead.</li>
+                        <li>A team will have participate in 5 modules.</li>
+                        <li>From the 5 modules the team has to choose, they must choose at least one module from each of the following
+                            categories.
+                            <br />
+                            <ul type="disc">
+                                <li>Category 1: Sciences (any 1 of 4)
+                                    <ul type="circle">
+                                        <li>Kashish-e- Khla</li>
+                                        <li>Reyaziat</li>
+                                        <li>Elixir Vitae</li>
+                                        <li>Theodosius Apoptosis</li>
+                                    </ul>
+                                </li>
+
+                                <li>Category 2: Engineering (any 1 of 4)
+                                    <ul type="circle">
+                                        <li>Autobot</li>
+                                        <li>TronCity</li>
+                                        <li>Trojan Construe</li>
+                                        <li>Codedash</li>
+                                    </ul>
+                                </li>
+
+                                <li>Category 3: General (any 1 of 4)
+                                    <ul type="circle">
+                                        <li>Kryptos Chamber</li>
+                                        <li>Six Feet Under</li>
+                                        <li>Multiverse</li>
+                                        <li>Eco-lution</li>
+                                    </ul>
+                                </li>
+                            </ul>
+                            The remaining 2 can be chosen from any of modules left.
+                        </li>
+
+                        <li>The team lead must check their email for payment details soon after they fill the registration form.</li>
+                        <li>The registration will be finalized once the team uploads their paymet confirmation receipt on the account
+                            portal/ emails the receipt at <i>atomos@habib.edu.pk</i></li>
+                        <li>The form entries will be saved to complete it later on, hence the team must fill the form at once.</li>
+                    </ol>
 
                 </Modal.Body>
             </Modal>
